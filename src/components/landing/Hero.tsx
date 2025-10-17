f511bdd9bffe5d4f5414108876840d20a3644b0e
Kevin lima <kevinguimaraesdelima8@gmail.com>
2025-10-17 15:21:51 +0000

    remova a cor amarela de fundo e coloque uma linha colorida em formato de

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
             <br />
             Uma{' '}
             <span className="relative inline-block">
-              <span className="relative z-10 bg-yellow-400 px-2 text-primary rounded-md">Fonte de Renda:</span>
+              <span className="relative z-10 text-white rounded-md">Fonte de Renda:</span>
               <svg
                 className="absolute left-0 -bottom-2 w-full h-2 text-yellow-400"
                 xmlns="http://www.w3.org/2000/svg"
