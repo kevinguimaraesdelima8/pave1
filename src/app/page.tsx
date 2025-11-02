721a1c1b8d65f8dd0a018c53b97568871ca9be0a
Kevin lima <kevinguimaraesdelima8@gmail.com>
2025-11-02 01:56:14 +0000

    crie mais uma sessão igual a essa (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
         <YouWillLearn />
         <Testimonials />
         <TestimonialScreenshots />
+        <YouWillLearn />
         <Bonus />
         <SocialProof />
         <Offer />
